e4e9ddf5f66227859fb94540df22092449689594
Karol Kowalczyk <KKowalczyk@dialogdesign.ca>
2019-05-02 15:07:32 -0600

    Initial shell design for the project, folowing somewhat what was discussed with Dan in Vancouver.

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,54 +8,34 @@ class Layout extends React.Component {
     const rootPath = `${__PATH_PREFIX__}/`
     let header
 
-    if (location.pathname === rootPath) {
-      header = (
-        <h1
-          style={{
-            ...scale(1.5),
-            marginBottom: rhythm(1.5),
-            marginTop: 0,
-          }}
-        >
+    header = (
+      <div className="chrome">
+        <div className="header">
           <Link
-            style={{
-              boxShadow: `none`,
-              textDecoration: `none`,
-              color: `inherit`,
-            }}
-            to={`/`}
-          >
-            {title}
-          </Link>
-        </h1>
-      )
-    } else {
-      header = (
-        <h3
           style={{
-            fontFamily: `Montserrat, sans-serif`,
-            marginTop: 0,
+            boxShadow: `none`,
+            textDecoration: `none`,
+            color: `inherit`,
+            display: `collapsed`,
           }}
+          to={`/`}
         >
-          <Link
-            style={{
-              boxShadow: `none`,
-              textDecoration: `none`,
-              color: `inherit`,
-            }}
-            to={`/`}
-          >
-            {title}
-          </Link>
-        </h3>
-      )
-    }
+          <img 
+            src="\header_image.png" 
+            />
+            </Link>
+            <div className="seperator"  >
+              energy analysis explorer
+            </div>
+        </div>
+  </div> 
+
+    )
     return (
       <div
         style={{
           marginLeft: `auto`,
           marginRight: `auto`,
-          maxWidth: rhythm(24),
           padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
         }}
       >
