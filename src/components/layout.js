d75cdb279bebc1c6aa345e26a51dfa42beabfb1a
Jason Masters <JMasters@dialogdesign.ca>
2019-05-13 17:22:40 -0400

    primarily changing the Navigation import to work on linux  #315 -

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,5 @@
 import React from "react" 
-import Navigation from './Navigation'
+import Navigation from './navigation'
 import styled, { ThemeProvider, createGlobalStyle  } from "styled-components"
 import { FaInstagram, FaLinkedin, FaYoutube, FaTwitter } from 'react-icons/fa'
 import theme from "../../config/theme" 
